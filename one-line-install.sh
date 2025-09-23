9dfffcc667bb84cbedc5dc2182adfd0682a1d422
Dan13681989 <139562189+Dan13681989@users.noreply.github.com>
2025-09-24 02:55:42 +0330

    chore: Auto-update 2025-09-24

--- one-line-install.sh
+++ one-line-install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # One-line CleanMac installer
-echo "🔧 Installing CleanMac via one-line command..."
 bash <(curl -sSL https://raw.githubusercontent.com/Dan13681989/cleanmac/main/install.sh)
